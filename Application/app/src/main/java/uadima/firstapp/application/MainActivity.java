6609f2a3f1d3bcd132a1cb5cb53352715627b9d1
uadima <uadima.hire@gmail.com>
2016-01-02 13:28:44 +0200

    Скобки
    Cраные скобки

--- Application/app/src/main/java/uadima/firstapp/application/MainActivity.java
+++ Application/app/src/main/java/uadima/firstapp/application/MainActivity.java
@@ -113,6 +113,17 @@ public class MainActivity extends AppCompatActivity {
         client.disconnect();
     }
 
+    private final static Character plus = '+';
+    private final static Character minus = '-';
+    private final static Character divide = '/';
+    private final static Character multiply = '*';
+    private final static Character openbreacket = '(';
+    private final static Character closebreacket = ')';
+    private int temp = 0;
+    private int temp2 = 0;
+    String nextreserved = new String();
+
+
     public void calcButtonOnClick(View view) {
         text.setText(text.getText().toString() + ((Button) view).getText().toString());
     }
@@ -123,65 +134,127 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calcOnClick(View view) {
-        calc();
+        try {
+            calc();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+        }
     }
 
     private void calc() {
         String str = text.getText().toString();
-        double sum = calc(str);
+        temp = 0;
+        int sum = phasebreacket(str);
         Log.d(TAG, sum + " результат ");
     }
 
-    private double calc(String next) {
+    private int phasebreacket(String next) {
+        Log.d(TAG, "получил на обработку строку " + next);
+        boolean flag = false;
+        if (temp != 0 && nextreserved.length() < 2) {
+            Log.d(TAG, "длина оставшейся строки минимальна отдаю на выход ");
+            return (Integer.parseInt(next));
+        }
+        if (temp != 0) {
+            String beforeSubStr = nextreserved.substring(0, temp);
+            String afterSubStr = nextreserved.substring(temp);
+            next=beforeSubStr+next+afterSubStr;
+            nextreserved=""; // дабы не зациклится удалим склееную строку
+            beforeSubStr=""; // дабы не зациклится удалим склееную строку
+            afterSubStr=""; // дабы не зациклится удалим склееную строку
+        }
+        if (temp == 0) {
+       //     Log.d(TAG, "получил на обработку строку " + next + " -1ая итерация");
+        }
+        for (int i = 0; i < next.toCharArray().length; i++) {
+            if (openbreacket.equals(next.toCharArray()[i])) {
+                temp = i;
+                for (int x = i; x < next.toCharArray().length; x++) {
+                    if (closebreacket.equals(next.toCharArray()[x])) {
+                        temp2=x;
+                        nextreserved = next.substring(0, temp) + next.substring(x + 1);
+                        Log.d(TAG, "оставляю в памяти строку " + nextreserved);
+                        next = next.substring(temp + 1, x);
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if (flag) {
+            Log.d(TAG, "выражение в скобках " + next);
+        } else Log.d(TAG, "выражений в скобках не найдено");
+        return calc(next);
+    }
+
+
+    private int calc(String next) {
         List<String> res1 = new ArrayList<>();
         List<String> action = new ArrayList<>();
-        int i = 0;
-        int j = 0;
-        for (Character c : next.toCharArray()) {
+        for (int i = 0; i < next.toCharArray().length; i++) {
             if (i + 1 == next.length()) {
                 res1.add(next.substring(1));
-                Log.d(TAG, j + 1 + "oe " + " последнее значение = " + res1.get(res1.size() - 1));
+                Log.d(TAG, action.size() + 1 + "oe " + " последнее значение = " + res1.get(res1.size() - 1));
             }
-            if (c.equals('+') || c.equals('-') || c.equals('/') || c.equals('*')) {
-                if (c.equals('+')) {
+            if (plus.equals(next.toCharArray()[i]) || minus.equals(next.toCharArray()[i]) || divide.equals(next.toCharArray()[i]) || multiply.equals(next.toCharArray()[i]) || openbreacket.equals(next.toCharArray()[i]) || closebreacket.equals(next.toCharArray()[i])) {
+                if (plus.equals(next.toCharArray()[i])) {
                     action.add("plus");
                 }
-                if (c.equals('-')) {
+                if (minus.equals(next.toCharArray()[i])) {
                     action.add("minus");
                 }
-                if (c.equals('/')) {
+                if (divide.equals(next.toCharArray()[i])) {
                     action.add("divide");
                 }
-                if (c.equals('*')) {
+                if (multiply.equals(next.toCharArray()[i])) {
                     action.add("multiply");
                 }
-                if (res1.size() == 0) {
+                if (action.size() == 1) {
                     res1.add(next.substring(0, i));
                 } //в случае когдамы первый раз входим в if делаем сдвиг на 1 единицу влево, по скльку цифре не предшествует арифм знак
-                if (res1.size() > 0) {
+                if (action.size() > 1) {
                     res1.add(next.substring(1, i));
                 }
                 next = next.substring(i);
-                Log.d(TAG, j + 1 + " значение = " + res1.get(res1.size() - 1));
+                Log.d(TAG, action.size() + " значение = " + res1.get(res1.size() - 1));
                 Log.d(TAG, action.get(action.size() - 1) + " действие");
-                j++;
                 i = 0; //длина пройденной строки,обнуляется в случае нахождения арифм операции
             }
-            i++;
         }
-        return phaseToo(res1, action, j);
+        return phasetwo(res1, action);
     }
 
-    private double phaseToo(List<String> res1, List<String> action, int j) {
-        double sum = 0;
-        for (int f = 0; f <= j; f++) {
-            for (int x = 0; x <= j; x++) {
+
+    /**
+     * private double phasetest(List<String> res1, List<String> action)
+     * {
+     * for (int x = 0; x <= action.size()-1; x++)
+     * {
+     * if (x==action.size()-1)
+     * {
+     * Log.d(TAG, x+1 + " значение = " + res1.get(x) );
+     * Log.d(TAG, " действие " + action.get(x) );
+     * Log.d(TAG, x+2 + " последнее значение = " + res1.get(x+1) );
+     * }else {
+     * Log.d(TAG, x+1 + " значение = " + res1.get(x) );
+     * Log.d(TAG, " действие " + action.get(x) );
+     * }
+     * }
+     * return phaseToo(res1, action);
+     * }
+     */
+
+
+    private int phasetwo(List<String> res1, List<String> action) {
+        for (int f = 0; f <= action.size() - 1; f++) {
+            for (int x = 0; x <= action.size() - 1; x++) {
                 if (action.get(f) == "divide") {
-                    Log.d(TAG, sum + " делю " + res1.get(f) + "на" + res1.get(f + 1));
+                    Log.d(TAG, " делю " + res1.get(f) + "на" + res1.get(f + 1));
                     res1.set(f, Integer.toString(Integer.parseInt(res1.get(f)) / Integer.parseInt(res1.get(f + 1))));
-                    Log.d(TAG, sum + " получаю " + res1.get(f));
-                    for (int t = f; t <= j; t++) {
-                        if (t == j) {
+                    Log.d(TAG, " получаю " + res1.get(f));
+                    for (int t = f; t <= action.size() - 1; t++) {
+                        if (t == action.size() - 1) {
                             res1.set(t + 1, "");
                             action.set(t, "");
                         } else {
@@ -192,8 +265,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if (action.get(f) == "multiply") {
                     res1.set(f, Integer.toString(Integer.parseInt(res1.get(f)) * Integer.parseInt(res1.get(f + 1))));
-                    for (int t = f; t <= j; t++) {
-                        if (t == j) {
+                    for (int t = f; t <= action.size() - 1; t++) {
+                        if (t == action.size() - 1) {
                             res1.set(t + 1, "");
                             action.set(t, "");
                         } else {
@@ -204,11 +277,12 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-        return phaseThree(res1, action, j, sum);
+        return phaseThree(res1, action);
     }
 
-    private double phaseThree(List<String> res1, List<String> action, int j, double sum) {
-        for (int f = 0; f <= j; f++) {
+    private int phaseThree(List<String> res1, List<String> action) {
+        int sum = 0;
+        for (int f = 0; f <= action.size() - 1; f++) {
             if (action.get(f) == "plus") {
                 if (f == 0) {
                     sum = Integer.parseInt(res1.get(f)) + Integer.parseInt(res1.get(f + 1));
@@ -225,10 +299,11 @@ public class MainActivity extends AppCompatActivity {
                     sum = sum - Integer.parseInt(res1.get(f + 1));
                 }
             }
-            if (f == j && sum == 0) {
+            if (f == action.size() - 1 && sum == 0) {
                 sum = Integer.parseInt(res1.get(0));
             }
         }
-        return sum;
+        return phasebreacket(Integer.toString(sum));
     }
+
 }
